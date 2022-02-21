f0ee4c2cd7fdd565f6bb6c1ef4f73eedde5d84c6
Santiago <santiagorojas.plc@gmail.com>
2022-02-21 20:03:00 -0300

    tarjeta responsiv

--- frontend/src/components/card/cardServ.js
+++ frontend/src/components/card/cardServ.js
@@ -6,7 +6,7 @@ import imgs from "../imgs/img";
 function CardServ({name, img, description, price } ){
     return (
         <div className={style.frameCard}>
-            <img src={img} ></img>
+            <img src={img} className={style.imgserv} ></img>
             <div>
             <h2>{name}</h2>
             <h6>categorías:</h6>
@@ -14,7 +14,7 @@ function CardServ({name, img, description, price } ){
             <p>{description}</p>
             <h3>{price}</h3>  ARS 
             <div className={style.here}>
-            <img src={imgs.here}></img> <h6>Fiambalá</h6>
+            <img src={imgs.here} ></img> <h6>Fiambalá</h6>
             </div>
             </div>
         </div>
