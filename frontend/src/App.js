473884de12a5cd366209a5021f53cfeb593adb00
clara <bernasconiclara00@gmail.com>
2022-02-25 13:00:28 -0300

    nada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,21 @@
-/* import React from 'react';
+import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 
 import {
   Footer,
-  Navbar
+  Navbar,
+  NavbarHome
 } from './components';
 
 import { 
   Login,
-  Home
+  Services,
+  Home,
+
 } from './pages';
 
 import './App.scss';
-// import { AiFillPropertySafety } from 'react-icons/ai';
-
-function Layout({children}) {
-  return (
-    <>
-      <Navbar />
-      {children}
-      <Footer />
-    </>    
-  )
-} 
+import Detail from './pages/serviceDetail/detail';
 
 function App() {
 
@@ -30,32 +23,17 @@ function App() {
 
   return (
     <div className="App">
-
-      {(pathname !== '/login' && pathname !== '/') ? <Navbar /> : ""}
-        
+      {(pathname !== '/') ? <Navbar /> : <NavbarHome />}
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path='/login' element={<Login />} />
-        
-        <Route path="/" element={<Navbar />}>
-          <Route path='products' element={<Products />} />
-          <Route path='services' element={<Services />} />
-          <Route path='login' element={<Login />} />
-        </Route>
-
-        <Route element={<Layout />}>
-          <Route path='/products' element={<Products />} />
-          <Route path='services' element={<Services />} />
-          <Route path='/login' element={<Login />} />
-        </Route>
+        <Route path='/products' element={<Login />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/detail' element={<Detail />} />
       </Routes>
-
-      {
-        pathname === '/login' || <Footer />
-      }
-
+      <Footer />
     </div>
   )
 }
 
-export default App; */
+export default App;
